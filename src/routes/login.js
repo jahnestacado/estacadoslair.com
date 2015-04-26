d0f434a4dcd06b6cbfb287578e33199d0dab0052
Jahn <ioannis.kiwe@gmail.com>
2015-04-26 16:52:15 +0200

    src/routes/login.js: Now retrieves user credentials from db

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,30 +1,28 @@
 var express = require("express");
+var dbConnection = require("./../db/init.js").dbConnection;
 var loginRouter = express.Router();
-
-var username = "jahn";
-var password = "jahn";
-
-function isPasswordValid(username, password) {
-
-}
 
 function generateSessionId() {
     return new Date().getTime().toString();
 }
 
-loginRouter.post('/', function(request, response) {
-    var status;
+function initUserSession(request, response) {
+    request.session[request.body.username] = generateSessionId();
+    response.cookie("sessionId", request.session[request.body.username]);
+    response.cookie("username", request.body.username);
+}
 
-    if (request.body.username === username && request.body.password === password) {
-        request.session[request.body.username] = generateSessionId();
-        response.cookie("sessionId", request.session[request.body.username]);
-        response.cookie("username", request.body.username);
-        status = 200; //OK
-    } else {
-        status = 400; //Bad request
-    }
-    
-    response.sendStatus(status);
+loginRouter.post('/', function(request, response) {
+    dbConnection.collection("users").findOne({username: request.body.username}, function(error, result) {
+        var status = 400; //Bad request
+        // Intentional loose comparison for numbers as a String
+        if (result.password == request.body.password) {
+            initUserSession(request, response);
+            status = 200; //OK
+        }
+
+        response.sendStatus(status);
+    });
 });
 
 module.exports = loginRouter;
