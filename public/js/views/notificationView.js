66ba19b5c277419f26291ac035bec3ec547822bc
Jahn <ioannis.kiwe@gmail.com>
2015-06-14 11:51:07 +0200

    public/js/views/notificationView.js: Changed fading-out interval of notification messages

--- public/js/views/notificationView.js
+++ public/js/views/notificationView.js
@@ -3,26 +3,24 @@ require([
     "underscore",
     "backboneExtended",
     "text!notificationTemplate",
-], function($, _, Backbone, viewTemplate) {
+], function ($, _, Backbone, viewTemplate) {
 
     var NotificationView = Backbone.View.extend({
-        initialize: function() {
+        initialize: function () {
             var view = this;
             Backbone.bus.on("notification", view.render, view);
         },
         el: "body",
         template: _.template(viewTemplate),
-        render: function(message) {
+        render: function (message) {
             var view = this;
             var statusClass = view.statusCssClass[message.status];
             var notification = $.extend(message, {statusClass: statusClass});
 
             $(view.template(notification))
                     .appendTo(view.$el)
-                    .fadeIn(4000, function() {
-                        $(this).fadeOut(6000, function() {
-                            $(this).remove();
-                        });
+                    .fadeOut(5000, function () {
+                        $(this).remove();
                     });
         },
         statusCssClass: {
