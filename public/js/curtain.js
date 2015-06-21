f9a267b31b5b08520e93cbff06859e25c9a08c0d
Jahn <ioannis.kiwe@gmail.com>
2015-06-21 21:10:03 +0200

    public/js/curtain.js: Refactored curtain resizing mechanism to handle different screen resolutions

--- public/js/curtain.js
+++ public/js/curtain.js
@@ -1,102 +1,116 @@
-define(["jquery"], function($) {
+define(["jquery"], function ($) {
 
     var BORDER_SIZE = 1;
-    var CURTAIN_LEFT_WIDTH = 250;
-    var CURTAIN_RIGHT_WIDTH = $(window).width() - CURTAIN_LEFT_WIDTH - BORDER_SIZE * 4;
-    var CURTAIN_HEIGHT = $(window).height();
-    
-    initialize();
 
-    function initialize(){
-         initializeLeftCurtain();
-         initializeRightCurtain();
-         $("#open-btn").click(function() { open(); });
-         $("#back-btn").click(function() { close(); });
-    }
-   
-    function initializeLeftCurtain() {
-        $("<div/>").appendTo("#home-page")
-                .attr("id", "curtain-left")
-                .addClass("curtain-left-closed")
-                .height(CURTAIN_HEIGHT)
-                .css({
-                    left: "-" + (CURTAIN_LEFT_WIDTH + 4) + "px",
-                    border: BORDER_SIZE + "px solid black"});
-
-        $("#curtain-left").on('transitionend webkitTransitionEnd', function(e) {
-            var leftCurtain = $(e.target);
-
-            if (leftCurtain.hasClass("curtain-left-closed")) {
-                $(e.target).css("display", "none");
-            }
+    var curtainJs = function curtainJs() {
+        renderLeftCurtain();
+        renderRightCurtain();
+        $("#open-btn").click(function () {
+            open();
         });
-    }
-    
-    function initializeRightCurtain() {
-        $("<div/>").appendTo("#home-page")
-                .attr("id", "curtain-right")
-                .addClass("curtain-right-closed")
-                .height(CURTAIN_HEIGHT)
-                .css({
-                    right: "-" + (CURTAIN_RIGHT_WIDTH + 4) + "px",
-                    border: BORDER_SIZE + "px solid black"});
-
-        $("#curtain-right").on('transitionend webkitTransitionEnd', function(e) {
-            var rightCurtain = $(e.target);
-
-            if (rightCurtain.hasClass("curtain-right-closed")) {
-                $(e.target).css("display", "none");
-            }
+        $("#back-btn").click(function () {
+            close();
         });
-    }
 
-    $(window).on("resize", function() {
+        return controls;
+    };
+
+    $(window).on("resize", function () {
         controls.close();
-        var leftCurtainElQ = $("#curtain-left");
-        var rightCurtainElQ = $("#curtain-right");
-        CURTAIN_HEIGHT = $(window).height();
-
-        leftCurtainElQ.height(CURTAIN_HEIGHT);
-        rightCurtainElQ.height(CURTAIN_HEIGHT);
+        renderLeftCurtain();
+        renderRightCurtain();
     });
-
 
     var controls = {
         close: function close() {
             var leftCurtainElQ = $("#curtain-left");
             var rightCurtainElQ = $("#curtain-right");
+            var curtainWidths = getCurtainWidths();
+
 
             leftCurtainElQ
                     .removeClass("curtain-left-opened")
                     .addClass("curtain-left-closed")
-                    .css("left", "-" + (CURTAIN_LEFT_WIDTH + BORDER_SIZE * 2) + "px");
+                    .css("left", "-" + curtainWidths.left + "px");
 
             rightCurtainElQ
                     .removeClass("curtain-right-opened")
                     .addClass("curtain-right-closed")
-                    .css("right", "-" + (CURTAIN_RIGHT_WIDTH + BORDER_SIZE * 2) + "px");
+                    .css("right", "-" + curtainWidths.right + "px");
         },
         open: function open() {
             var leftCurtainElQ = $("#curtain-left");
             var rightCurtainElQ = $("#curtain-right");
-
-            CURTAIN_RIGHT_WIDTH = $(window).width() - CURTAIN_LEFT_WIDTH - BORDER_SIZE * 4;
+            var curtainWidths = getCurtainWidths();
 
             leftCurtainElQ
                     .removeClass("curtain-left-closed")
                     .css("display", "")
                     .addClass("curtain-left-opened")
-                    .width(CURTAIN_LEFT_WIDTH)
+                    .width(curtainWidths.left)
                     .css("left", "0");
 
             rightCurtainElQ
                     .removeClass("curtain-right-closed")
                     .css("display", "")
                     .addClass("curtain-right-opened")
-                    .width(CURTAIN_RIGHT_WIDTH)
+                    .width(curtainWidths.right)
                     .css("right", "0");
         }
     };
 
-    return controls;
+    var renderLeftCurtain = function renderLeftCurtain() {
+        $("<div/>").appendTo("#home-page")
+                .attr("id", "curtain-left")
+                .addClass("curtain-left-closed")
+                .css({
+                    left: "-" + getCurtainWidths().left + "px",
+                    border: BORDER_SIZE + "px solid black"});
+
+        $("#curtain-left").on('transitionend webkitTransitionEnd', function (e) {
+            var leftCurtain = $(e.target);
+
+            if (leftCurtain.hasClass("curtain-left-closed")) {
+                $(e.target).css("display", "none");
+            }
+        });
+    };
+
+    var renderRightCurtain = function renderRightCurtain() {
+        $("<div/>").appendTo("#home-page")
+                .attr("id", "curtain-right")
+                .addClass("curtain-right-closed")
+                .css({
+                    right: "-" + getCurtainWidths().right + "px",
+                    border: BORDER_SIZE + "px solid black"});
+
+        $("#curtain-right").on('transitionend webkitTransitionEnd', function (e) {
+            var rightCurtain = $(e.target);
+
+            if (rightCurtain.hasClass("curtain-right-closed")) {
+                $(e.target).css("display", "none");
+            }
+        });
+    };
+
+    var getCurtainWidths = function getCurtainWidths() {
+        var curtainWidths = {};
+        var screenWidth = $(window).width();
+        var leftCurtainHorizontalScreenRatio = null;
+
+        if (screenWidth > 1100 && screenWidth < 1600) {
+            leftCurtainHorizontalScreenRatio = 0.19;
+        } else if (screenWidth > 700) {
+            leftCurtainHorizontalScreenRatio = 0.17;
+        } else {
+            leftCurtainHorizontalScreenRatio = 0.35;
+        }
+
+        curtainWidths.left = screenWidth * leftCurtainHorizontalScreenRatio;
+        curtainWidths.right = (screenWidth * (1 - leftCurtainHorizontalScreenRatio)) - BORDER_SIZE * 4;
+
+        return curtainWidths;
+    };
+
+    return curtainJs();
 });
