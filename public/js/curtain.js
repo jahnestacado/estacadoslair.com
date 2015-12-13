d3740356ae316aba47a8a5f9f73001ef3def3f84
Jahn <ioannis.kiwe@gmail.com>
2015-12-13 13:23:10 +0100

    public/js/curtain.js: Increased animation duration

--- public/js/curtain.js
+++ public/js/curtain.js
@@ -1,5 +1,6 @@
 define(["curtainjs","jquery"], function (c, $) {
     var curtain = $("#home-page").curtainify({
+            animationDuration: 500,
         landscape:{
             widthRatioCurtainA : 0.15,
             widthRatioCurtainB : 0.85,
