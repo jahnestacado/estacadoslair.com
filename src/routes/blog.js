a3fcd5c1e4ef7ee6284b3381e495c1bfaf70a83c
Jahn <ioannis.kiwe@gmail.com>
2015-12-13 13:24:10 +0100

    src/routes/blog.js: Now all responses return the results from db action as json

--- src/routes/blog.js
+++ src/routes/blog.js
@@ -8,27 +8,27 @@ var path = "/articles";
 
 blogRouter.get(path, function(request, response) {
     blogCollection.find().toArray(function(error, results) {
-        response.send(results);
+        response.status(200).json(results);
     });
 });
 
 blogRouter.get(path + "/:id", function(request, response) {
     blogCollection.findOne({_id: mongoskin.helper.toObjectID(request.params.id)}, function(error, results) {
-        response.send(results);
+        response.status(200).json(results);
     });
 });
 
 blogRouter.post(path, auth, function(request, response) {
     blogCollection.insert(request.body, function(error, results) {
         if (results) {
-            response.send("Added");
+            response.status(200).json(results);
         }
     });
 });
 
 blogRouter.delete(path + "/:id", auth, function(request, response) {
     blogCollection.remove({_id: mongoskin.helper.toObjectID(request.params.id)}, function(error, results) {
-        response.sendStatus(200);
+        response.status(200).json(results);
     });
 });
 
@@ -36,7 +36,7 @@ blogRouter.put(path + "/:id", auth, function(request, response) {
     var id = mongoskin.helper.toObjectID(request.params.id);
     delete request.body._id;
     blogCollection.update({_id: id}, request.body, function(error, results) {
-        response.sendStatus(200);
+        response.status(200).json(results);
     });
 });
 
