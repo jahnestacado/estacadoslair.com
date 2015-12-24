256aaaa60200d6f5885087d016c51dd433b0d6e5
Jahn <ioannis.kiwe@gmail.com>
2015-12-24 01:03:33 +0100

    public/js/views/blogPostListView.js: Implemented swipe-post functionality and adjusted styling for portrait mode

--- public/js/views/blogPostListView.js
+++ public/js/views/blogPostListView.js
@@ -4,9 +4,7 @@ define([
     "text!listViewTemplate",
     "blogPostView",
     "curtain",
-
 ], function ($, Backbone, viewTemplate, BlogPostView, CURTAIN) {
-
     var BlogPostListView = Backbone.View.extend({
         el: ".curtain-A",
         template: _.template(viewTemplate),
@@ -14,6 +12,7 @@ define([
             var view = this;
             view.blogPostView = new BlogPostView();
             view.blogPosts = blogPosts;
+            $.fn.swipe.defaults.excludedElements = "button, input, select, textarea, .noSwipe";
         },
         render: function (blogId) {
             var view = this;
@@ -30,6 +29,13 @@ define([
                     }
                     view.renderBlogPost(blogId);
                 }
+
+                view.$el.find(".list-group li").swipe( {
+                    swipe:function(event, direction) {
+                        view.swipeBlogListItem(event, direction);
+                    },
+                    threshold:0
+                });
             };
 
             if(!view.blogPosts.length){
@@ -49,17 +55,42 @@ define([
                 }
             });
         },
+        swipeBlogListItem: function(event,direction){
+            var view = this;
+            event.stopImmediatePropagation();
+            var listElQ = $(event.target).parent();
+
+            if(listElQ.hasClass("active")){
+                listElQ.removeClass("active");
+                var activeItemIndex = listElQ.index();
+                var listItemElQs = listElQ.siblings();
+                var numOfItems = listItemElQs.length;
+
+                if(direction === "right" && activeItemIndex + 1 === numOfItems){
+                    view.selectBlogPost(listItemElQs.first());
+                } else if(direction === "left" && activeItemIndex === 0){
+                    view.selectBlogPost(listItemElQs.last());
+                } else{
+                    var action = direction === "right" ? "next" : "prev";
+                    view.selectBlogPost($(listElQ[action]()));
+                }
+            }
+        },
         renderBlogPost: function (id) {
             var view = this;
             if (id) {
-                $("#" + id).addClass("active");
+                $("#" + id).parent()
+                        .addClass("active")
+                        .fadeOut(0)
+                        .fadeIn(50);
+
                 var selectedBlogPost = view.getModelFromCollection(id);
                 view.blogPostView.render(selectedBlogPost);
             }
         },
         events: {
             "click #back-home": "loadHomePage",
-            "click .list-group a": "selectBlogPost"
+            "click .list-group li": "onSelectBlogPost",
         },
         getModelFromCollection: function(id){
             var view = this;
@@ -69,11 +100,14 @@ define([
             CURTAIN.close();
             require("routes").navigate("/", {trigger: true});
         },
-        selectBlogPost: function (event) {
-            var targetElQ = $(event.target);
-            var id = targetElQ.attr("id");
+        onSelectBlogPost: function (event) {
             var view = this;
-
+            var targetElQ = $(event.target).parent();
+            view.selectBlogPost(targetElQ);
+        },
+        selectBlogPost: function (listElQ) {
+            var view = this;
+            var id = listElQ.find("a").attr("id");
             require("routes").navigate("#" + view.getPathDomain() + "/" + id, {trigger: true});
         },
         getPathDomain: function () {
