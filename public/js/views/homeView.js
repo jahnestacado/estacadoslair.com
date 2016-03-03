f967c5c22fe499b1e6a4394b3dce59a90e9fbd26
Jahn <ioannis.kiwe@gmail.com>
2016-03-03 21:40:52 +0100

    public/js/views/homeView.js: Decreased fadein duration

--- public/js/views/homeView.js
+++ public/js/views/homeView.js
@@ -29,7 +29,7 @@ define([
         },
         fadeInHome: function() {
             var view = this;
-            view.$el.children().fadeOut(0).fadeIn(1200)
+            view.$el.children().fadeOut(0).fadeIn(800);
         },
         logoChangeOnHover: function() {
             var view = this;
