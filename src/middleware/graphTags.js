71548feddd385d84c74452ae68a5a0a56d36a132
Jahn <ioannis.kiwe@gmail.com>
2016-04-04 00:49:07 +0200

    src/middleware/graphTags.js: Added getDescription function and improved error handling

--- src/middleware/graphTags.js
+++ src/middleware/graphTags.js
@@ -1,25 +1,40 @@
 var dbUtils = require("./../db/blog-collection-utils.js");
 var Q = require("q");
 var DynamicGraphTags = require("./../templates/dynamic-graph-tags.js");
+var createError = require("http-errors");
+var sanitizeHtml = require("sanitize-html");
+var DESCRIPTION_LENGTH = 200;
 
 module.exports =  function addGraphTags(request, response, next){
     var refererUrl = request.session.referer;
+    delete request.session.referer;
+
     var deferred = Q.defer();
     var promise =  deferred.promise;
-    if(refererUrl && /^(\/blog\/)/.test(refererUrl)){
+    if(refererUrl && /^(\/blog\/)/.test(refererUrl) && refererUrl.split("/").length === 4){
         var id = refererUrl.match(/\/blog\/(.*)\/.*/)[1];
-        dbUtils.findOne(id, function(results){
-            results.url = "https://" + request.get("host") + refererUrl;
-            deferred.resolve(DynamicGraphTags(results));
+        dbUtils.findOne({id: id}, function(results){
+            if(results){
+                results.url = "https://" + request.get("host") + refererUrl;
+                results.description = getPostDescription(results.body);
+                deferred.resolve(DynamicGraphTags(results));
+            } else{
+                deferred.reject(createError(404));
+            }
         }, deferred.reject)
 
         promise.then(function(graphTags){
             request.graphTags = graphTags;
             next();
-        }, next);
+        }, function(error){
+            response.sendStatus(error.status || 500);
+        });
     } else{
         next();
     }
-
-    delete request.session.referer;
 };
+
+function getPostDescription(body){
+    var sanitizedHtml = sanitizeHtml(body, {allowedTags: [], allowedAttributes: []});
+    return sanitizedHtml.substring(0, DESCRIPTION_LENGTH);
+}
