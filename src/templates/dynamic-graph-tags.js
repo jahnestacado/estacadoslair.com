2eadded58b43c5048b67b6275f8c3854cc67621e
Jahn <ioannis.kiwe@gmail.com>
2016-04-04 00:47:55 +0200

    src/templates/dynamic-graph-tags.js: Added description tag

--- src/templates/dynamic-graph-tags.js
+++ src/templates/dynamic-graph-tags.js
@@ -1,7 +1,9 @@
 var _ = require("underscore");
-var dynamicGraphTags = '\n'+
+var dynamicGraphTags = '<!-- <Dynamic graph-tags> -->\n'+
 '   <meta property="og:title" content="<%= post.title %>"/>\n'+
-'   <meta property="og:url" content="<%= post.url %>"/>';
+'   <meta property="og:description" content="<%= post.description %>"/>\n'+
+'   <meta property="og:url" content="<%= post.url %>"/>\n'+
+'<!-- </Dynamic graph-tags> -->';
 
 var DynamicGraphTags = function(post){
     return _.template(dynamicGraphTags)({post:post});
