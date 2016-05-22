7cdc737fd8affc1c2f194c998e84bbfe46d484ac
Jahn <ioannis.kiwe@gmail.com>
2016-05-22 22:05:51 +0200

    src/db/config.js: Now mongoURI is derived from env var MONGO_URI if present

--- src/db/config.js
+++ src/db/config.js
@@ -1,10 +1,10 @@
 var mongoskin = require("mongoskin");
-var mongoUrl = process.env.MONGO || "localhost:27017";
-var dbConnection = mongoskin.db("mongodb://" + mongoUrl + "/mywebsite");
+var mongoURI = process.env.MONGO_URI || "localhost:27017";
+var dbConnection = mongoskin.db("mongodb://" + mongoURI + "/mywebsite");
 var _ = require("underscore");
 var COLLECTIONS = ["blog", "users"]
 
-dbConnection.collections(function(erroe, collections) {
+dbConnection.collections(function(error, collections) {
     var collectionNames = collections.map(function(collection) {
         return collection.collectionName;
     });
