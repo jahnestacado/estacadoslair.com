8859646a49525a48e32f1186ee929c3af43e7cb5
Jahn <ioannis.kiwe@gmail.com>
2016-08-05 22:57:33 +0200

    public/js/backboneExtend.js: Updated to promise-then style ajax calls

--- public/js/backboneExtend.js
+++ public/js/backboneExtend.js
@@ -1,18 +1,18 @@
-define(["backbone"], function (Backbone) {
+define(["backbone", "jquery"], function (Backbone, $) {
     Backbone.bus = $.extend({}, Backbone.Events);
 
     Backbone.View = $.extend(Backbone.View, {
         onAccessGranted: function (callbacks) {
-            $.get("/auth/isAuthorized")
-                    .success(function () {
-                        callbacks.onSuccess && callbacks.onSuccess();
-                    })
-                    .error(function () {
-                        callbacks.onFailure && callbacks.onFailure();
-                    });
-        }
+            $.get("/auth/isAuthorized").then(
+                function success(){
+                    callbacks.onSuccess && callbacks.onSuccess();
+                },
+                function error(){
+                    callbacks.onFailure && callbacks.onFailure();
+                });
+            }
+        });
+
+
+        return Backbone;
     });
-
-
-    return Backbone;
-});
